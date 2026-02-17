dcbdbf5cb0d032bd1472c21b911011fc2177f08f
maths <matheusalan.dev@gmail.com>
2026-02-17 20:03:26 -0300

    cookbook 16

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({ baseURL: 'https://blogpessoal-gen-pbvi.onrender.com' });
 
-export const cadastrarUsuario = async (url: string, dados: Object , setDados: Function) => {
+export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados);
     setDados(resposta.data);
 }
